183263ef7808fb8bb579954c1ace34eb1b0e18fa
RibeiroRenan2024 <ribeirorenan294@yahoo.com.br>
2025-12-05 05:16:58 -0300

    Criando treixo do código que atualiza o excel consolidado

--- tests/extrato_up.py
+++ tests/extrato_up.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import time
 import os
-#from scripts.functions.mover_arquivo import verificar_e_remover_arquivo
+import win32com.client
+import pythoncom
 
 st.set_page_config(page_title="extrato-sos", layout="wide")
 st.title("📊 Extrato-SOS - Resultado Financeiro")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PASTA_ARQUIVOS = os.path.join(BASE_DIR, "..", "src", "extrato_sos", "arquivos")
+PASTA_CONSOLIDADO = os.path.join(BASE_DIR, "..", "src", "extrato_sos", "consolidado")
 
+# Garante que a pasta existe
 os.makedirs(PASTA_ARQUIVOS, exist_ok=True)
-
 
 uploaded_files = st.file_uploader(
     "📎 Carregue um ou mais extratos (CSV ou Excel)",
@@ -27,9 +29,52 @@ if uploaded_files:
         if os.path.isfile(caminho_final):
             os.remove(caminho_final)
 
-        # Salva
+        # Salva o novo
         with open(caminho_final, "wb") as f:
             f.write(uploaded_file.getbuffer())
 
         st.success(f"Arquivo salvo! {nome_arquivo}")
+
+    print("================================")
+    print("Arquivos carregados com sucesso!")
+    print("================================")
+
+    time.sleep(5)
+    print()
+    print("Atualizando consolidado...")
+    print()
+
+    # Nome fixo do consolidado
+    nome_arquivo_consolidado = "consolidado.xlsx"
+    caminho_final_consolidado = os.path.join(PASTA_CONSOLIDADO, nome_arquivo_consolidado)
+
+    # =====================================================
+    # 🔧 Inicializa ambiente COM para Excel
+    pythoncom.CoInitialize()
+
+    try:
+        # Inicia o excel
+        excel = win32com.client.Dispatch("Excel.Application")
+        excel.Visible = 0  # roda em background
+
+        # Abre o consolidado
+        workbook = excel.Workbooks.Open(caminho_final_consolidado)
+
+        # Atualiza dados
+        workbook.RefreshAll()
+
+        excel.CalculateUntilAsyncQueriesDone()
+        #Save
+        workbook.Save()
+
+
+    finally:
+        # Fecha Excel e descarta recursos COM
+        excel.Quit()
+        pythoncom.CoUninitialize()
+
+    print("================================")
+    print("Consolidado atualizado!")
+    print("================================")
+
 st.stop()
